06d585eec970cb259858874ede7d6081415b02bf
Leonardo Cabezas Grajales <numeraljoker@gmail.com>
2022-10-22 12:43:44 -0500

    se crea maquetacion da la app en html

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const months = document.querySelector ("#months")
+const weeks = document.querySelector ("#weeks")
+const days = document.querySelector ("#days")
+const hours = document.querySelector ("#hours")
+const minutes = document.querySelector ("#minutes")
+const seconds = document.querySelector ("#seconds")
